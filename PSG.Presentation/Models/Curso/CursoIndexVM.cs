3ad53f63e376a59092320891bbbbe0dc2624edcb
fhelps32 <fellepe32iolo@gmail.com>
2026-08-07 17:58:03 -0300

    vamo claude

---
+++ PSG.Presentation/Models/Curso/CursoIndexVM.cs
@@ -0,0 +1,37 @@
+﻿namespace PSG.Presentation.Models.Curso
+{
+    public class CursoIndexVM
+    {
+        public List<CursoItem> Cursos { get; set; } = new List<CursoItem>();
+        public CursoDetalhes CursoDetalhes { get; set; } = new CursoDetalhes();
+    }
+
+    public class CursoItem
+    {
+        public string Nome { get; set; }
+        public string Sigla { get; set; }
+        public string NomeCoordenador { get; set; }
+        public int QuantidadeAlunos { get; set; }
+        public int TaxaCancelamento { get; set; }
+    }
+
+    public class CursoDetalhes
+    {
+        string Nome { get; set; }
+        string Sigla { get; set; }
+        public int QuantidadeAlunos { get; set; }
+        public int QuantidadeAlunosCancelados { get; set; }
+        public int QuantidadeAlunosReprovados { get; set; }
+        public int TaxaCancelamento { get; set; }
+        public int TaxaReprovacao { get; set; }
+
+        public List<ModuloCursoItem> Modulos { get; set; } = new List<ModuloCursoItem>();
+    }
+
+    public class ModuloCursoItem
+    {
+        public string Nome { get; set; }
+        public string NomeProfessor { get; set; }
+        public int QuantidadeAlunos { get; set; }
+    }
+}
